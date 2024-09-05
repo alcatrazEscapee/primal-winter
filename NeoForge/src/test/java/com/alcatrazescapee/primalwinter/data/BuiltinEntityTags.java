f71357e10afb0287b87c4a4d29407afc4f739005
AlcatrazEscapee <alex@molleroneill.com>
2024-09-05 12:35:56 -0400

    Make more hostile mobs powder snow immune and walkable

--- NeoForge/src/test/java/com/alcatrazescapee/primalwinter/data/BuiltinEntityTags.java
+++ NeoForge/src/test/java/com/alcatrazescapee/primalwinter/data/BuiltinEntityTags.java
@@ -17,21 +17,20 @@ public final class BuiltinEntityTags extends EntityTypeTagsProvider
     @Override
     protected void addTags(HolderLookup.Provider provider)
     {
-        tag(EntityTypeTags.FREEZE_IMMUNE_ENTITY_TYPES).add(
-            EntityType.WOLF,
-            EntityType.WANDERING_TRADER,
-            EntityType.TRADER_LLAMA,
-            EntityType.PILLAGER,
-            EntityType.ILLUSIONER,
-            EntityType.VINDICATOR,
-            EntityType.WITCH);
-        tag(EntityTypeTags.POWDER_SNOW_WALKABLE_MOBS).add(
-            EntityType.WOLF,
-            EntityType.WANDERING_TRADER,
-            EntityType.TRADER_LLAMA,
-            EntityType.PILLAGER,
-            EntityType.ILLUSIONER,
-            EntityType.VINDICATOR,
-            EntityType.WITCH);
+        tag(EntityTypeTags.FREEZE_IMMUNE_ENTITY_TYPES)
+            .addTag(EntityTypeTags.UNDEAD)
+            .addTag(EntityTypeTags.RAIDERS)
+            .add(
+                EntityType.SPIDER,
+                EntityType.CREEPER);
+        tag(EntityTypeTags.POWDER_SNOW_WALKABLE_MOBS)
+            .addTag(EntityTypeTags.UNDEAD)
+            .addTag(EntityTypeTags.RAIDERS)
+            .add(
+                EntityType.WOLF,
+                EntityType.WANDERING_TRADER,
+                EntityType.TRADER_LLAMA,
+                EntityType.SPIDER,
+                EntityType.CREEPER);
     }
 }
